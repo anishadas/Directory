051e5595a624efa66beb3b52e5afc2b9ccc59178
Anisha <payalad10@gmail.com>
2024-01-05 21:16:47 +0530

    updated readme file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://anishadas.github.io/Directory/"
+  plugins: [react()]
 })
